159d0fe271a7f184ca36e7654d99dcbe828e4f35
Kaczmarczyk.Michal <209344@edu.p.lodz.pl>
2019-05-16 16:44:46 +0200

    added setUp and shouldThrowWrongMoneyAmountExceptionWhenMoneyIsZero test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -1,15 +1,56 @@
 package edu.iis.mto.testreactor.exc3;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AtmMachineTest {
+
+    private CardProviderService cardService;
+    private BankService bankService;
+    private MoneyDepot moneyDepot;
+
+    private AtmMachine atmMachine;
+
+    private Payment payment;
+
+    private Money money;
+    private Card card;
+    private AuthenticationToken authenticationToken;
+
+    @Before
+    public void setUp() {
+        bankService = mock(BankService.class);
+        cardService = mock(CardProviderService.class);
+        moneyDepot = mock(MoneyDepot.class);
+
+        atmMachine = new AtmMachine(cardService, bankService, moneyDepot);
+    }
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
     }
+
+    @Test(expected = WrongMoneyAmountException.class)
+    public void shouldThrowWrongMoneyAmountExceptionWhenMoneyIsZero() {
+        money = Money.builder()
+                .withAmount(0)
+                .withCurrency(Currency.PL)
+                .build();
+
+        card = Card.builder()
+                .withCardNumber("numer")
+                .withPinNumber(1)
+                .build();
+
+        atmMachine.withdraw(money, card);
+    }
+
 
 }
